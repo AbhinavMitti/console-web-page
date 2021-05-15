db1e0efd6b5e8932d662e60145e5f8d73f62dc87
AbhinavMitti <77564368+AbhinavMitti@users.noreply.github.com>
2021-05-15 14:48:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var first_name = "Abhinav ";
+
+var last_name = "Mitti";
+
+var square_root = Math.sqrt(16);
+
+var full_name = first_name.concat(last_name);
+
+var maximum_number = Math.max(7, 14);
+
+var minimum_number = Math.max(7, 21);
+
+console.log("My first console project");
+
+console.log(full_name);
+
+console.log(square_root);
+
+console.log(maximum_number);
+
+console.log(minimum_number);
